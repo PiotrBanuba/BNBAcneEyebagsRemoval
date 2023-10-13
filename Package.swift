95803b210784737ab457b1a351d2578ae3ab3e28
Piotr Kulbaka <petr.kulbaka@banuba.com>
2023-10-13 09:52:02 +0300

    1.8.0-10

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let bnbPackageVersion: Version = "1.8.0-9"
+let bnbPackageVersion: Version = "1.8.0-10"
 
 let package = Package(
     name: "BNBAcneEyebagsRemoval",
@@ -13,10 +13,10 @@ let package = Package(
             name: "BNBAcneEyebagsRemoval",
             targets: [
                 "BNBAcneEyebagsRemoval",
-                "BNBAcneEyebagsRemovalBNBSdkCore",
-                "BNBAcneEyebagsRemovalBNBEffectPlayer",
-                "BNBAcneEyebagsRemovalBNBScripting",
-                "BNBAcneEyebagsRemovalBNBFaceTracker"
+                "BNBSdkCore",
+                "BNBEffectPlayer",
+                "BNBScripting",
+                "BNBFaceTracker"
             ]
         ),
     ],
@@ -43,42 +43,6 @@ let package = Package(
             name: "BNBAcneEyebagsRemoval",
             url: "https://d2cm7wtcqqw29.cloudfront.net/1.8.0-58-g46079e7d74/BNBAcneEyebagsRemoval.zip",
             checksum: "0559658787637a1e42b41e1927468faabe06f62fc10fe51e5cf7b09f21d21f09"
-        ),
-        .target(
-            name: "BNBAcneEyebagsRemovalBNBSdkCore",
-            dependencies: [
-                .product(
-                    name: "BNBSdkCore",
-                    package: "BNBSdkCore"
-                ),
-            ]
-        ),
-        .target(
-            name: "BNBAcneEyebagsRemovalBNBEffectPlayer",
-            dependencies: [
-                .product(
-                    name: "BNBEffectPlayer",
-                    package: "BNBEffectPlayer"
-                ),
-            ]
-        ),
-        .target(
-            name: "BNBAcneEyebagsRemovalBNBScripting",
-            dependencies: [
-                .product(
-                    name: "BNBScripting",
-                    package: "BNBScripting"
-                ),
-            ]
-        ),
-        .target(
-            name: "BNBAcneEyebagsRemovalBNBFaceTracker",
-            dependencies: [
-                .product(
-                    name: "BNBFaceTracker",
-                    package: "BNBFaceTracker"
-                ),
-            ]
         ),
     ]
 )
